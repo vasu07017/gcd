0180d7738dc5f266734e1dea9e7affd4e163d772
vasu07017 <118751603+vasu07017@users.noreply.github.com>
2024-12-29 20:50:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+  
+    alert(`Thank you, ${name}! We have received your message.`);
+  });
+  const fadeInElements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+fadeInElements.forEach(el => observer.observe(el));
